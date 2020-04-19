1f4519efb01561f505ef138388de4a43a5317779
lemonadexx <lemonadexx@users.noreply.github.com>
2020-04-19 18:25:59 +0800

    members ver. view updated

--- js-members/html_actuator.js
+++ js-members/html_actuator.js
@@ -62,7 +62,8 @@ HTMLActuator.prototype.addTile = function (tile) {
   this.applyClasses(wrapper, classes);
 
   inner.classList.add("tile-inner");
-  inner.textContent = tile.value;
+  //inner.textContent = tile.value;
+  inner.innerHTML = "<img src=\"https://lemonadexx.github.io/play-seventeen/assets/ot13/"+tile.value+".jpg\" style=\"width:100%;\">";
 
   if (tile.previousPosition) {
     // Make sure that the tile gets rendered in the previous position first
