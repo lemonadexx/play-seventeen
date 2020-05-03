c6de627b65fdf067cfe84a7a4407b2d6b33380dd
lemonadexx <lemonadexx@users.noreply.github.com>
2020-05-03 13:41:10 +0800

    summoncnt for albums++ updated
    four new tiles are added after every move

--- js/application-albums.js
+++ js/application-albums.js
@@ -1,5 +1,5 @@
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
   //new GameManager(5, 8192, 2, KeyboardInputManager, HTMLActuator, LocalScoreManager);
-  new GameManager(6, 32768, 2, KeyboardInputManager, HTMLActuator, LocalStorageManager, "albumsfull");
+  new GameManager(6, 32768, 4, KeyboardInputManager, HTMLActuator, LocalStorageManager, "albumsfull");
 });
--- js/local_storage_manager.js
+++ js/local_storage_manager.js
@@ -51,6 +51,15 @@ LocalStorageManager.prototype.setBestScore = function (score) {
 // Game state getters/setters and clearing
 LocalStorageManager.prototype.getGameState = function () {
   var stateJSON = this.storage.getItem(this.gameStateKey);
+  console.log(stateJSON);
+  return stateJSON ? JSON.parse(stateJSON) : null;
+};
+
+LocalStorageManager.prototype.debugger = function () {
+  var statetext = {"grid":{"size":6,"cells":[[{"position":{"x":0,"y":0},"value":4},{"position":{"x":0,"y":1},"value":2},null,null,null,null],[{"position":{"x":1,"y":0},"value":8},{"position":{"x":1,"y":1},"value":4},null,null,null,null],[{"position":{"x":2,"y":0},"value":32},{"position":{"x":2,"y":1},"value":8},{"position":{"x":2,"y":2},"value":4},{"position":{"x":2,"y":3},"value":2},{"position":{"x":2,"y":4},"value":4},{"position":{"x":2,"y":5},"value":2}],[{"position":{"x":3,"y":0},"value":128},{"position":{"x":3,"y":1},"value":32},{"position":{"x":3,"y":2},"value":16},{"position":{"x":3,"y":3},"value":8},null,null],[{"position":{"x":4,"y":0},"value":1024},{"position":{"x":4,"y":1},"value":64},{"position":{"x":4,"y":2},"value":32},{"position":{"x":4,"y":3},"value":16},{"position":{"x":4,"y":4},"value":4},null],[{"position":{"x":5,"y":0},"value":65536},{"position":{"x":5,"y":1},"value":4},{"position":{"x":5,"y":2},"value":64},{"position":{"x":5,"y":3},"value":32},{"position":{"x":5,"y":4},"value":16},{"position":{"x":5,"y":5},"value":4}]]},"score":982116,"over":false,"won":false,"keepPlaying":false};
+  this.storage.setItem(this.gameStateKey, JSON.stringify(statetext));
+  var stateJSON = this.storage.getItem(this.gameStateKey);
+  console.log(stateJSON);
   return stateJSON ? JSON.parse(stateJSON) : null;
 };
 
