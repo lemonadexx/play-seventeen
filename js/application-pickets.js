d4012d8cc9488bbb3a6bbfd1580938fbfe606e82
lemonadexx <lemonadexx@users.noreply.github.com>
2020-05-18 22:42:19 +0800

    Update application-pickets.js

--- js/application-pickets.js
+++ js/application-pickets.js
@@ -1,5 +1,5 @@
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
   //new GameManager(5, 8192, 2, KeyboardInputManager, HTMLActuator, LocalScoreManager);
-  new GameManager(5, 32768, 2, KeyboardInputManager, HTMLActuator, LocalStorageManager, "members");
+  new GameManager(5, 32768, 2, KeyboardInputManager, HTMLActuator, LocalStorageManager, "pickets");
 });
